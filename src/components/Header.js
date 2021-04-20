7974bf7342ab35d28ef78702aa5eb904b885445e
Dion Kapfumvuti <dionkapf@gmail.com>
2021-04-20 21:23:21 +0300

    Turn header into functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from "../assets/logo.png";
 import "./Header.css";
 import "./hamburger.css";
 
-class Header extends Component {
-  handleMenuClick() {
+function Header() {
+  const handleMenuClick = () => {
     document.querySelector(".hamburger").classList.toggle("is-active");
-  }
-  render() {
-    return (
-      <div className="header">
-        <h1>
-          <img
-            src={logo}
-            alt="Todoer Logo"
-            style={{ width: "8vw", height: "8vw" }}
-          />
-          Todoer
-        </h1>
-        <button
-          className="hamburger hamburger--minus"
-          type="button"
-          onClick={this.handleMenuClick}
-        >
-          <span className="hamburger-box">
-            <span className="hamburger-inner"></span>
-          </span>
-        </button>
-      </div>
-    );
-  }
+  };
+  return (
+    <div className="header">
+      <h1>
+        <img
+          src={logo}
+          alt="Todoer Logo"
+          style={{ width: "8vw", height: "8vw" }}
+        />
+        Todoer
+      </h1>
+      <button
+        className="hamburger hamburger--minus"
+        type="button"
+        onClick={handleMenuClick}
+      >
+        <span className="hamburger-box">
+          <span className="hamburger-inner"></span>
+        </span>
+      </button>
+    </div>
+  );
 }
 
 export default Header;
