a82b281007e5e1ec23f1c4a1b399d7d784032b2f
Dion Kapfumvuti <dionkapf@gmail.com>
2021-04-20 21:48:05 +0300

    Turn Todos into a functional component

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,22 +1,22 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import TodoItem from "./TodoItem.js";
-class Todos extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: props.todos,
-    };
-  }
 
-  render() {
-    return (
-      <div>
-        {this.state.todos.map((todo, index) => {
-          return <TodoItem key={index} todo={todo} />;
-        })}
-      </div>
-    );
-  }
+function Todos(props) {
+  const [todos, setTodos] = useState(props.todos);
+
+  const addNewTodo = (todo) => {
+    setTodos({
+      ...todos,
+      todo,
+    });
+  };
+
+  return (
+    <div>
+      {todos.map((todo, index) => {
+        return <TodoItem key={index} todo={todo} />;
+      })}
+    </div>
+  );
 }
-
 export default Todos;
