18fe622e25a1823e89b862a2f7e1b4b537b54c84
Dion Kapfumvuti <dionkapf@gmail.com>
2021-04-20 21:48:24 +0300

    Turn TodoItem into a functional component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,49 +1,34 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./TodoItem.css";
-class TodoItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todo: props.todo.text,
-      tag: props.todo.tag,
-      active: false,
-    };
-    this.toggleActive = this.toggleActive.bind(this);
-  }
 
-  toggleActive() {
-    const currentState = this.state.active;
-    this.setState({ active: !currentState });
-  }
+function TodoItem(props) {
+  const [text, setText] = useState(props.todo.text);
+  const [tag, setTag] = useState(props.todo.tag);
+  const [active, setActive] = useState(false);
 
-  render() {
-    return (
-      <span
+  const toggleActive = (currentActive) => {
+    setActive(!currentActive);
+  };
+
+  return (
+    <span
+      className={active ? "container neumorphic-container_active" : "container"}
+    >
+      <button
+        type="button"
         className={
-          this.state.active
-            ? "container neumorphic-container_active"
-            : "container"
+          active
+            ? "btn neumorphic-btn neumorphic-checkbox neumorphic-checkbox_active"
+            : "btn neumorphic-btn neumorphic-checkbox"
         }
-      >
-        <button
-          type="button"
-          className={
-            this.state.active
-              ? "btn neumorphic-btn neumorphic-checkbox neumorphic-checkbox_active"
-              : "btn neumorphic-btn neumorphic-checkbox"
-          }
-          onClick={this.toggleActive}
-        ></button>
-        <p className="todo">{this.state.todo}</p>
-        <div className="mid-container">
-          <p className={this.state.active ? "tag-active border" : "tag border"}>
-            {this.state.tag}
-          </p>
-          <div className="material-icons close">close</div>
-        </div>
-      </span>
-    );
-  }
+        onClick={toggleActive}
+      ></button>
+      <p className="todo">{text}</p>
+      <div className="mid-container">
+        <p className={active ? "tag-active border" : "tag border"}>{tag}</p>
+        <div className="material-icons close">close</div>
+      </div>
+    </span>
+  );
 }
-
 export default TodoItem;
