a23dce78215ca15f6ac0d21860b6fe6070bdbc8e
Dion Kapfumvuti <dionkapf@gmail.com>
2021-04-21 00:00:15 +0300

    Add TodosContext

--- src/contexts/TodosContext.js
+++ src/contexts/TodosContext.js
@@ -1,11 +1,6 @@
 import React, { createContext, useState } from "react";
 
-export const TodosContext = createContext([
-  { text: "Cook the dishes", tag: "HOME" },
-  { text: "Read The Sun Will Rise Again (and cry)", tag: "STUDY" },
-  { text: "Eat push ups", tag: "PERSONAL" },
-  { text: "Synergise rough draft", tag: "ACADEMIC" },
-]);
+export const TodosContext = createContext();
 
 const TodosContextProvider = (props) => {
   const [todos, setTodos] = useState([
