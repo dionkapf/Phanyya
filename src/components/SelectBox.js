eace77485ded7ae2ba83ffdcbe2815e3f1df285b
Dion Kapfumvuti <dionkapf@gmail.com>
2021-04-21 22:34:11 +0300

    Turn SelectBox into a functional component

--- src/components/SelectBox.js
+++ src/components/SelectBox.js
@@ -1,35 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import "./SelectBox.css";
 
-class SelectBox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: props.name,
-      items: props.items,
-    };
-    this.onSelectChange = this.onSelectChange.bind(this);
-  }
+function SelectBox(props) {
+  const onSelectChange = (e) => {
+    const tag = props.items.tags[parseInt(e.target.value) - 1].trim();
+    props.getTag(tag);
+  };
 
-  onSelectChange(e) {
-    const tag = this.state.items.tags[parseInt(e.target.value) - 1].trim();
-    this.props.getTag(tag);
-  }
-
-  render() {
-    return (
-      <select id="tags" onChange={this.onSelectChange}>
-        <option value="0">{this.props.name}</option>
-        {this.state.items.tags.map((tag, index) => {
-          return (
-            <option className="select-item" value={index + 1} key={index + 1}>
-              {tag.toUpperCase()}
-            </option>
-          );
-        })}
-      </select>
-    );
-  }
+  return (
+    <select id="tags" onChange={onSelectChange}>
+      <option value="0">{props.name}</option>
+      {props.items.tags.map((tag, index) => {
+        return (
+          <option className="select-item" value={index + 1} key={index + 1}>
+            {tag.toUpperCase()}
+          </option>
+        );
+      })}
+    </select>
+  );
 }
-
 export default SelectBox;
